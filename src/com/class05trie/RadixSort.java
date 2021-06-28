787a549f6b80a4f1f9b2080c69552a0e556a1bc6
sweetyang123 <84315764+sweetyang123@users.noreply.github.com>
2021-06-28 10:16:22 +0800

    基数排序

--- src/com/class05trie/RadixSort.java
+++ src/com/class05trie/RadixSort.java
@@ -1,7 +1,18 @@
 package com.class05trie;
 
+import java.util.Arrays;
+
 /**
  * 基数排序
+ * 思想：
+ * （给10个桶，最先找到数组中最大数，并算出最大数的位数i（需循环进桶的次数），
+ * 先将个位相同的数放到同一个桶里，个位数按序依次放入桶（队列，先进先出的）中，再根据顺序出桶放到帮助数组help中，
+ * 再将十位进出桶，百位，千位。。。。知直到最大位i）
+ * 实现：
+ * 数组实现，给一个大小为10的数组count，先分别算出个位的数，如count[0]的值为个位为0的个数。。。。；
+ * 再将count数组中的值依次往后累加，如count[1]=count[0]+count[1]；
+ * 最后再将数组从右到左方向根据count数组放入新数组中，得到的新数组则就为个位有序的了；
+ * 依次再将十位，百位。。。的数这样操作
  */
 public class RadixSort {
     //获取数组中最大数的位数
@@ -22,6 +33,14 @@ public class RadixSort {
     private static int getDigit(int num,int d){
         return (num/(int)Math.pow(10,d-1))%10;
     }
+
+    /**
+     *
+     * @param arr
+     * @param l
+     * @param r
+     * @param digit 最大值的位数
+     */
     private static void radixSort(int[] arr,int l,int r,int digit){
         if (arr==null||arr.length<2)return;
         int[] help = new int[r-l+1];
@@ -29,16 +48,16 @@ public class RadixSort {
         for (int i = 1; i <=digit ; i++) {
             int[] count = new int[10];
 //            分别得到个位相同的数的个数。。。
-            for (int j = l; j < r; j++) {
+            for (int j = l; j <= r; j++) {
                 int digit1 = getDigit(arr[j], i);
                 count[digit1]++;
             }
 //            从前往后累加，拿到<=0,<=1,<=2。。。的个数
-            for (int j = 1; j <=count.length; j++) {
+            for (int j = 1; j <count.length; j++) {
                 count[j]=count[j]+count[j-1];
             }
 //            将数组从右到左反向将值在count辅助下个位有序。。。的放到help中
-            for (int j = r-1; j >=l; j++) {
+            for (int j = r; j >=l; j--) {
                 int digit1 = getDigit(arr[j], i);
                 help[count[digit1]-1]=arr[j];
                 count[digit1]--;
@@ -47,6 +66,60 @@ public class RadixSort {
                 arr[j]=help[j];
             }
         }
+    }
+
+    //test
+    private static boolean isEqual(int[] arr1,int[] arr2){
+        if ((arr1==null&&arr2!=null)||(arr1!=null&&arr2==null))return false;
+        if (arr1.length!=arr2.length)return false;
+        if (arr1==null&&arr2==null)return true;
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i]!=arr2[i])return false;
+        }
+        return true;
+    }
+    private static int[] randomArr(int maxValue, int maxSize) {
+        int size = (int)(Math.random()*maxSize+1);
+        int[] arr=new int[size];
+        for (int i = 0; i <size ; i++) {
+            arr[i]=(int)(Math.random()*maxValue+1);
+        }
+        return arr;
+    }
+    private static void print(int[] arr){
+        if (arr==null)return;
+        for (int i = 0; i <arr.length ; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println(" ");
+    }
+    private static int[] copyArr(int[] arr){
+        if (arr==null)return null;
+        int[] arr1 = new int[arr.length];
+        for (int i = 0; i <arr.length ; i++) {
+            arr1[i]=arr[i];
+        }
+        return arr;
+    }
+    private static void comparator(int[] arr){
+        Arrays.sort(arr);
+    }
+    public static void main(String[] args) {
+        int testTime=50000;
+        int maxValue=100;
+        int maxSize=100;
+        for (int i = 0; i <testTime ; i++) {
+            int[] arr1=randomArr(maxValue,maxSize);
+            int[] arr2=copyArr(arr1);
+            radixSort(arr1,0,arr1.length-1,getSize(arr1));
+            comparator(arr2);
+            if (!isEqual(arr1,arr2)){
+                print(arr1);
+                print(arr2);
+                return;
+            }
+        }
+        System.out.println("success");
     }
 }
 
