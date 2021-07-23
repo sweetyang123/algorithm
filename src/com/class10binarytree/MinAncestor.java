0749dc4b05e0af923a71209190bc0b22bd949cf9
sweetyang123 <84315764+sweetyang123@users.noreply.github.com>
2021-07-23 17:23:59 +0800

    最小公共祖先

---
+++ src/com/class10binarytree/MinAncestor.java
@@ -0,0 +1,11 @@
+package com.class10binarytree;
+
+/**
+ * 给一个头节点，两个未知节点X，Y，求X，Y的最小公共祖先
+ * ① 哈希表，hashSet
+ * ② 最初交汇点，是否发现X，Y，
+ * 在左边发现了则在左边。。。；
+ * 都没发现但发现了X，Y则当前节点为最小祖先
+ */
+public class MinAncestor {
+}
