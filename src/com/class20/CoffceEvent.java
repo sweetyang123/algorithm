8721a2b0a12357237799c5e746cef610eac83114
sweetyang123 <84315764+sweetyang123@users.noreply.github.com>
2021-08-22 21:36:02 +0800

    动态规划-coffee机洗杯子和自动挥发，寻求最短时间

--- src/com/class20/CoffceEvent.java
+++ src/com/class20/CoffceEvent.java
@@ -14,10 +14,69 @@ public class CoffceEvent {
         return process(arr,a,b,0,0);
     }
 
+    /**
+     *
+     * @param arr 每个人喝完的时间
+     * @param a 洗杯子的时间
+     * @param b 自然挥发的时间
+     * @param index
+     * @param washtime 洗的机器什么时候可以用
+     * @return
+     */
     private static int process(int[] arr, int a, int b, int index, int washtime) {
+        if (a>b)return 0;
         int N = arr.length;
         if (N -1==index){
+//            最后一个人喝完分自然挥发还是机洗
+           return Math.min(arr[index]+b, Math.max(washtime,arr[index])+a);
         }
-        return 0;
+        //不是最后一杯则分当前杯是机洗还是自然挥发；
+//        其中还要index和index后面的都要洗完
+        //index机洗时间
+        int wash=Math.max(washtime,arr[index])+a;
+        //index....机洗时间
+        int next1=process(arr,a,b,index+1,wash);
+//        取最大是因为最长时间为全部都洗完
+        int p1 = Math.max(wash,next1);
+        //挥发
+        int next2 = process(arr,a,b,index+1,washtime);
+        int p2=Math.max(arr[index]+b, next2);
+        return Math.min(p2,p1);
+    }
+
+    private static int dp(int[] arr, int a, int b){
+        if (a>b)return 0;
+        int N = arr.length;
+        int limit =0;
+        for (int i = 0; i < N; i++) {
+            limit=Math.max(arr[i],limit)+a;
+        }
+        int[][] dp = new int[N][limit+1];
+        for (int washtime = 0; washtime <= limit; washtime++) {
+            dp[N-1][washtime]=Math.min(arr[N-1]+b, Math.max(washtime,arr[N-1])+a);
+        }
+        for (int index = N-2; index >=0 ; index--) {
+            for (int washtime = 0; washtime <=limit ; washtime++) {
+                int p1=Integer.MAX_VALUE;
+                int wash=Math.max(washtime,arr[index])+a;
+                //当index的时间小于最大limit时才能走机洗这种情况
+                if (wash<=limit){
+                    //index....机洗时间
+                    int next1=dp[index+1][wash];
+//        取最大是因为最长时间为全部都洗完
+                    p1 = Math.max(wash,next1);
+                }
+                //挥发
+                int next2 = dp[index+1][washtime];
+                int p2=Math.max(arr[index]+b, next2);
+                dp[index][washtime]=Math.min(p2,p1);
+            }
+        }
+       return dp[0][0];
+    }
+    public static void main(String[] args) {
+        int[] arr = new int[]{1,2,4};
+        System.out.println(coffce(arr,3,10));
+        System.out.println(dp(arr,3,10));
     }
 }
