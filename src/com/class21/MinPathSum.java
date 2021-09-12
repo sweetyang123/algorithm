6489f8c825e51b06b2d06a00460a0f586a3b7861
sweetyang123 <84315764+sweetyang123@users.noreply.github.com>
2021-09-12 14:17:09 +0800

    动态规划

--- src/com/class21/MinPathSum.java
+++ src/com/class21/MinPathSum.java
@@ -4,30 +4,64 @@ package com.class21;
  * 给定一个二维数组matrix，一个人必须从左上角出发，最后到达右下角
  * 沿途只可以向下或者向右走，沿途的数字都累加就是距离累加和
  * 返回最小距离累加和
+ *
+ * 不用递归，直接动态规划得到，二维数组和一维数组两种方法实现
  */
 public class MinPathSum {
     private static int minPathSum(int[][] matrix){
         if (matrix == null || matrix.length == 0 || matrix[0] == null || matrix[0].length == 0) {
             return 0;
         }
-        return process(matrix,0,matrix[0].length-1);
+        int N = matrix.length;
+        int M = matrix[0].length;
+        int[][] dp = new int[N][M];
+        dp[0][0]=matrix[0][0];
+        //第0横行只能是由右移得到，则从左到右直接累加
+        for (int i = 1; i < N; i++) {
+            dp[i][0]=dp[i-1][0]+matrix[i][0];
+        }
+        //第0竖行只能是由下移得到，则从上到下直接累加
+        for (int j = 1; j < M; j++) {
+            dp[0][j]=dp[0][j-1]+matrix[0][j];
+        }
+//        而中间的都可用左边和上边的最小累加得到
+        for (int i = 1; i < N; i++) {
+            for (int j = 1; j < M; j++) {
+                dp[i][j]=Math.min(dp[i-1][j],dp[i][j-1])+matrix[i][j];
+            }
+        }
+        return dp[N-1][M-1];
     }
 
-    private static int process(int[][] matrix, int x, int y) {
-        if (x<0||x>matrix.length-1||y<0||y>matrix[0].length-1)return 0;
-        if (x==matrix.length-1&&y==0)return matrix[x][y];
-//        在x轴上则只能向左移动
-        int p1;
-        if (y==0)p1=process(matrix,x-1,0)+matrix[x][0];
-//        在x轴上则只能向下移动
-        if (x==matrix.length-1)return matrix[matrix.length-1][y];
-
-        process(matrix,x+1,y);
-        process(matrix,x,y-1);
-
-        return matrix[x][y];
+    /**
+     * 从二维数组到一维数组的优化，
+     * 由于除了第一横行和竖行，
+     * 每行的值有依赖于他上面和左面的值
+     * 所以每次比较后更新数组
+     * @param matrix
+     * @return
+     */
+    private static int minPathSum2(int[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0] == null || matrix[0].length == 0) {
+            return 0;
+        }
+        int N = matrix.length;
+        int M = matrix[0].length;
+        int[] arr= new int[N];
+        arr[0]=matrix[0][0];
+        //一维数组初始化，第一行累加
+        for (int i = 1; i < N; i++) {
+            arr[i]=arr[i-1]+matrix[i][0];
+        }
+        //每次更新一维数组，与之前的值作比较
+        for (int i = 1; i < M; i++) {
+            arr[0]=arr[0]+matrix[0][i];
+            for (int j = 1; j < N ; j++) {
+                arr[j]=Math.min(arr[j],arr[j-1])+matrix[j][i];
+            }
+        }
+        return arr[N-1];
     }
-
     // for test
     public static int[][] generateRandomMatrix(int rowSize, int colSize) {
         if (rowSize < 0 || colSize < 0) {
@@ -57,7 +91,7 @@ public class MinPathSum {
         int colSize = 10;
         int[][] m = generateRandomMatrix(rowSize, colSize);
         System.out.println(minPathSum(m));
-//        System.out.println(minPathSum1(m));
+        System.out.println(minPathSum2(m));
 
     }
 }
--- src/com/class22/KillMonster.java
+++ src/com/class22/KillMonster.java
@@ -8,4 +8,94 @@ package com.class22;
  * 求K次打击之后，英雄把怪兽砍死的概率
  */
 public class KillMonster {
+    private static double killMonster(int N,int M,int K){
+        if (N<1||M<1||K<1)return 0;
+        return (double) ((double) process(N,M,K)/(double) Math.pow(M+1,K));
+    }
+
+    /**
+     *
+     * @param n 有n滴血
+     * @param m 每次砍m滴血
+     * @param k 还剩k刀
+     * @return
+     */
+    private static double process(int n, int m, int k) {
+        if (k==0)return n<=0?1:0;
+        //血量流完后还剩k刀，则剩下的都满足
+        if (n<=0)return Math.pow(m+1,k);
+        int way=0;
+        for (int i = 0; i <= m; i++) {
+            way+=process(n-i,m,k-1);
+        }
+        return way;
+    }
+    private static double dp(int n, int m, int k){
+        if (n<1||m<1||k<1)return 0;
+        double[][] dp = new double[n+1][k+1];
+        dp[0][0]=1;
+        for (int j = 1; j <=k; j++) {
+            dp[0][j]= Math.pow(m+1,k);
+            for (int i = 0; i <= n; i++) {
+                int way=0;
+                for (int l = 0; l <=m; l++) {
+                    if (i-l>=0){
+                        way+=dp[i-l][j-1];
+                    }else {
+//                        最后一次将血砍完后，剩下的刀数-1
+                        way+=Math.pow(m+1,j-1);
+                    }
+
+                }
+                dp[i][j]=way;
+            }
+        }
+        return (double) ((double)dp[n][k]/(double) Math.pow(m+1,k));
+    }
+
+    /**
+     * 优化中间的循环枚举，
+     * 相邻两个格子的值依赖的格子有重复的内容
+     * @param n
+     * @param m
+     * @param k
+     * @return
+     */
+    private static double dp2(int n, int m, int k){
+        if (n<1||m<1||k<1)return 0;
+        double[][] dp = new double[n+1][k+1];
+        dp[0][0]=1;
+        for (int j = 1; j <=k; j++) {
+            dp[0][j]= Math.pow(m+1,k);
+            for (int i = 1; i <= n; i++) {
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+                if (i-m-1>=0){
+                    dp[i][j]-=dp[i-m-1][j-1];
+                }else {
+                    dp[i][j]-=Math.pow(m+1,j-1);
+                }
+            }
+        }
+        return (double) ((double)dp[n][k]/(double) Math.pow(m+1,k));
+    }
+    public static void main(String[] args) {
+        int NMax = 10;
+        int MMax = 10;
+        int KMax = 10;
+        int testTime = 200;
+        System.out.println("测试开始");
+        for (int i = 0; i < testTime; i++) {
+            int N = (int) (Math.random() * NMax);
+            int M = (int) (Math.random() * MMax);
+            int K = (int) (Math.random() * KMax);
+            double ans1 = killMonster(N, M, K);
+            double ans2 = dp(N, M, K);
+            double ans3 = dp2(N, M, K);
+            if (ans1 != ans2 &&ans1 != ans3) {
+                System.out.println("Oops!");
+                break;
+            }
+        }
+        System.out.println("测试结束");
+    }
 }
